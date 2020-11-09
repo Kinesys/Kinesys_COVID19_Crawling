b74ca169f3d0415f1c06feebbb86feff63c8e273
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-11-09 15:29:19 +0900

    Update Kinesys COVID_19_Crawling.py

--- src/Kinesys COVID_19_Crawling.py
+++ src/Kinesys COVID_19_Crawling.py
@@ -15,3 +15,24 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 html = str(soup.select_one(".data_table.mgt16"))
 
+df = pd.read_html(html)[0]
+
+df.columns = ['지역', '국가', '도시', '격리중', '환자발생수(사망)']
+
+df2 = df['환자발생수(사망)'].str.extract(r'([\d.]+[ㄱ-힣\s\(\)]+[\d,]*')
+
+df = df.drop('환자발생수(사망)', axis = 1)
+
+df['환자발생수'] = df2[0].str.replace(',', '').apply(lambda s: s and int(s))
+
+df['환자사망수'] = df2[1].str.replace(',', '').apply(lambda s: s and int(s) or 0)
+
+
+print(df.shape)
+print(df.head())
+
+#파일 저장
+df.to_word("코로나.docx")
+df.to_hancom("코로나.hwp")
+df.to_csv("코로나.csv")
+df.to_excel("코로나.xlsx")
