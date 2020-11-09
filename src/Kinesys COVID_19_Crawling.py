94ccbf8dc67fcb54ae91aeb1f65b896f286cf435
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-11-09 14:55:36 +0900

    Update Kinesys COVID_19_Crawling.py

--- src/Kinesys COVID_19_Crawling.py
+++ src/Kinesys COVID_19_Crawling.py
@@ -1,15 +1,12 @@
-#Kinesys COVID_19_Crawling.py
+#Kinesys COVID_19
 import pandas as pd 
 import urllib,request as urllib
 
 site_url = "https://ncov.mohw.go.kr/bdBoardList_Real.do" #크롤링을 진행할 사이트 주소
-
-openner = urllib.build_opener()
-
-openner.addheaders = [('User_Agent', 'Mozilla/5.0')]
 
 params = {
     'brdId' : 1
     'brdGubun' : 14,
 }
 
+response = 
